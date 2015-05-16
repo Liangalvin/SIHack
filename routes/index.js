46e8108433aaa814d01d575af4b675b681680fb4
Alvin Liang <liangalvin@yahoo.com>
2015-05-16 17:44:15 -0400

    twitter api wont work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var Twitter = require('twitter');
+var client;
+
+fs.readFile('apikey.json', function(err, data){
+var text = JSON.parse(data);
+  if (err) {throw err;}
+
+  client = new Twitter({
+    consumer_key: text.apiKey,
+    consumer_secret: text.apiSecret,
+    access_token_key: text.token,
+    access_token_secret: text.tokenSecret
+  });
+
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // console.log(client);
+  client.stream('statuses/filter', {track: '#nygiants'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
+
+  res.render('../views/index.ejs', { msg: 'Express'});
 });
 
 module.exports = router;
